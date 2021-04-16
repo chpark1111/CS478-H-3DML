8c10df619ad0b63f7955555068dd9468ab764286
Chanhyeok <chpark1111@naver.com>
2021-04-16 14:48:45 +0000

    Apply .gitignore

--- 2d_sup.py
+++ 2d_sup.py
@@ -6,7 +6,7 @@ from torch.autograd.variable import Variable
 from tensorboard_logger import configure, log_value
 
 from utils import read_config
-#from Model.models import Encoder
+#from Model.model import CSGmodel
 
 config = read_config.Config("2dsup_config.yml")
 
@@ -18,10 +18,19 @@ configure("log/tensorboard/{}".format(model_name), flush_secs=5)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')
-file_handler = logging.FileHandler(
-    'log/logger/{}.log'.format(model_name), mode='w')
+file_handler = logging.FileHandler('log/logger/{}.log'.format(model_name), mode='w')
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
+logger.info(config.config)
 
 #My code
-#encoder = Encoder()
+'''
+net = CSGmodel().cuda()
+if torch.cuda.is_available():
+    print('GPU available')
+    net.cuda()
+
+if config.preload_model:
+    print(config.pretrain_modelpath, "Loaded")
+    net.load_state_dict(torch.load(config.pretrain_modelpath))
+'''
